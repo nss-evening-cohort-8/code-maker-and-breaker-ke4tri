0f95aa19be39c5c86232636c73a1fc23ed95896e
Wayne Collier <ke4tri@gmail.com>
2018-09-22 12:39:52 -0500

    added html, js files and updated readme

---
+++ main.js
@@ -0,0 +1,21 @@
+//let letters = [ "a","b","c","d","e","f","g","h","i","g","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+
+// letters.forEach(function(element) {
+//     console.log(element);
+//   });
+
+
+let word = 'Hello!';
+
+//var index = 4;
+
+//console.log('The character code ' + word.charCodeAt(index) + ' is equal to ' + word.charAt(index));
+// expected output: "The character code 113 is equal to q"
+
+
+for(i=0;i<word.length;i++){
+    let encodeWord = [];
+   // encodeWord.push(word.charCodeAt(i));
+   // console.log(word.charCodeAt(i) + ' is equal to ' + word.charAt(i));
+    encodeWord.push(encodeWord + word.charCodeAt([i]));
+};
